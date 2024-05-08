1e63049fb6de810cdd8559449591a98137c883fa
sametozalp <sametozalp0056@gmail.com>
2024-05-08 17:48:30 +0300

    Update telegram.ino

--- telegram/telegram.ino
+++ telegram/telegram.ino
@@ -15,6 +15,8 @@ int ledStatus = 0;
 
 String test_photo_url = "https://firebasestorage.googleapis.com/v0/b/uploadimageiot.appspot.com/o/iot%2Fcap.png?alt=media&token=42f500df-5cc1-4738-9210-bb22f6468f1f";
 
+int person = 0;
+
 void setup() {
 
   Serial.begin(9600);
@@ -33,7 +35,12 @@ void setup() {
   }
 
   Serial.println("Baglandi..");
-  
+
+  /*
+  for (int i = 2; i < 14; i++) {
+    pinMode(i, OUTPUT);
+  }
+  */
 }
 
 void loop() {  
@@ -44,11 +51,30 @@ void loop() {
     handleNewMessages(numNewMessages);
     numNewMessages = bot.getUpdates(bot.last_message_received + 1);
   }
+
+  person = random(14);
+  Serial.println(person);
+
+  if (person != 0) {
+    /*
+    for (int i = 2; i < person + 2; i++) {
+      digitalWrite(i, HIGH);
+    }
+    */
+  }
+
+  delay(1000);
+
+  //close_leds();
+}
+
+void close_leds() {
+  for (int i = 2; i < 14; i++) {
+    digitalWrite(i, LOW);
+  }
 }
 
 void handleNewMessages(int numNewMessages) {
-  Serial.print("handleNewMessages ");
-  Serial.println(numNewMessages);
 
   for (int i = 0; i < numNewMessages; i++) {
     String chat_id = bot.messages[i].chat_id;
@@ -84,7 +110,7 @@ void handleNewMessages(int numNewMessages) {
 
     if (text == "/start") {
       String welcome = "Hoşgeldin " + from_name + "!\n";
-      welcome += "/Bu bir kamera izleme sistemidir.\n";
+      welcome += "Bu bir kamera izleme sistemidir.\n";
       welcome += "Görüntüdeki kişi sayısını belirli zaman aralıklarıyla alabilir ve görüntüyü anlık çekebilirsin.\n";
       bot.sendMessage(chat_id, welcome, "Markdown");
     }
